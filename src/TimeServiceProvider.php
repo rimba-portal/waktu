8b069d189e2e4355f3457582cb4f3abf808ff48e
Faros <114036707+a-faros@users.noreply.github.com>
2026-09-18 10:08:40 +0800

    Release v1.4.4

--- src/TimeServiceProvider.php
+++ src/TimeServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rimba\Time;
 
 use Filament\Actions\Action;
-use Filament\Facades\Filament;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
 use Rimba\Base\Services\BitesServiceProvider;
